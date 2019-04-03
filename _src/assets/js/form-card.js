edbdbef128fe91b73b6f5754df746bf255d5deb3
iris paredes <irisparedesjimenez@gmail.com>
2019-04-03 12:02:17 +0200

    create event fill-card

--- _src/assets/js/form-card.js
+++ _src/assets/js/form-card.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('prueba');
+const input = document.querySelector('.input');
+const result =  document.querySelector('.result');
+
+const addText = function textCard() {
+    if (input.value === '') {
+        result.innerHTML = 'No me borres :-)';
+    } else {
+        result.innerHTML = input.value;
+    } 
+} 
+input.addEventListener('keyup', addText);
