6a22047587679e6e5d5d67d2d5a55478663ce2ba
iris paredes <irisparedesjimenez@gmail.com>
2019-04-03 12:54:56 +0200

    create event fill card for job

--- _src/assets/js/form-card.js
+++ _src/assets/js/form-card.js
@@ -11,3 +11,17 @@ const addText = function textCard() {
     } 
 } 
 input.addEventListener('keyup', addText);
+
+const input1 = document.querySelector('.input1');
+const result1 =  document.querySelector('.result1');
+
+const addText1 = function textCard1() {
+    if (input1.value === '') {
+        result1.innerHTML = 'No me borres :-)';
+    } else {
+        result1.innerHTML = input1.value;
+    } 
+} 
+input1.addEventListener('keyup', addText1);
+
+
