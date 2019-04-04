e9f43c80a86a3ca993987048bfd458ea830b71a1
veroena <veronica.enamorado@gmail.com>
2019-04-05 01:09:38 +0200

    Add JS for color palette

---
+++ _src/assets/js/color-palette.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// console.log('color');
+
+const preview = document.querySelector('.section__card');
+const radio1 = document.getElementById('settings__palette1');
+const radio2 = document.getElementById('settings__palette2');
+const radio3 = document.getElementById('settings__palette3');
+
+
+function changeColor(addedClass) {
+  preview.classList.remove('option1', 'option2', 'option3');
+  preview.classList.add(addedClass);
+}
+
+
+radio1.addEventListener('click', function() {changeColor('option1')});
+  
+radio2.addEventListener('click', function() {changeColor('option2')});
+  
+radio3.addEventListener('click', function() {changeColor('option3')});
