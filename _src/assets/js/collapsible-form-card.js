4a0c44c2b04b5312434948db21457709d96a1907
leireriel <leirericop@gmail.com>
2019-04-11 11:52:19 +0200

    finish collapsable sections and arrow movement, still doing default vision

--- _src/assets/js/collapsible-form-card.js
+++ _src/assets/js/collapsible-form-card.js
@@ -1,5 +1,9 @@
 'use strict';
 // modificar nombre variable acc
+
+const fill = document.querySelector('.fill__form');
+const share = document.querySelector('.share__collapsible--container');
+
 let collapsibleTitle = document.querySelectorAll('.collapsible__title');
 
 for (let i = 0; i < collapsibleTitle.length; i++) {
@@ -7,46 +11,7 @@ for (let i = 0; i < collapsibleTitle.length; i++) {
 }
 
 function collapse(e) {
-  //sobra??
-  //let collapsibleTitle = document.querySelectorAll('.collapsible__title');
   let titleClick = e.currentTarget.parentElement;
-  //console.log(e.currentTarget.parentElement);
   titleClick.classList.toggle('hidden');
-  
-
-
-
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-    /*for (let i = 0; i < collapsibleTitle.length; i++) {
-      let currentElement = collapsibleTitle[i];
-      let collapsibleContainer = 
-      document.querySelector('#'+currentElement.id+'-container');
-      let arrow = 
-      document.querySelector('#'+currentElement.id+'-arrow');
-      if (this.id === currentElement.id){
-          collapsibleContainer.style.maxHeight = collapsibleContainer.scrollHeight + "px";
-          arrow.classList.replace('fa-chevron-up', 'fa-chevron-down');
-      }else{
-        collapsibleContainer.style.maxHeight =  null;
-        arrow.classList.replace('fa-chevron-down', 'fa-chevron-up');
-      }
-    };
-  } */
-// sustituir this por currentTarget
