30c52e15f7805f1325686ee325c6ed7668a75b4c
iris paredes <irisparedesjimenez@gmail.com>
2019-04-11 16:23:14 +0200

    create function collapseAll fix code

--- _src/assets/js/collapsible-form-card.js
+++ _src/assets/js/collapsible-form-card.js
@@ -1,15 +1,28 @@
 'use strict';
 
-const fill = document.querySelector('.fill__form');
-const share = document.querySelector('.share__collapsible--container');
 let collapsibleTitle = document.querySelectorAll('.collapsible__title');
+const collapsibleContainer = document.querySelectorAll('.parent__container');
 
 for (let i = 0; i < collapsibleTitle.length; i++) {
   collapsibleTitle[i].addEventListener('click', collapse); 
 }
 
 function collapse(e) {
-  let titleClick = e.currentTarget.parentElement;
-  titleClick.classList.toggle('hidden');
-};
+  let trigger = e.currentTarget.parentElement;
+  
+ 
+  if (trigger.classList.contains('hidden')) {
+    collapseAll();
+    trigger.classList.remove('hidden');
+  } else {
+    collapseAll();
+  }
+}
+
+function collapseAll() {
+  for (let i = 0; i < collapsibleContainer.length; i++) {
+    collapsibleContainer[i].classList.add('hiddenAll');
+  }
+}
+
 
