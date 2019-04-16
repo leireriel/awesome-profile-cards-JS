57e8bcc355b2e1afe1798bf42879a38cb2722835
iris paredes <irisparedesjimenez@gmail.com>
2019-04-16 16:58:29 +0200

    add changes fetch and function

--- _src/assets/js/api.js
+++ _src/assets/js/api.js
@@ -10,35 +10,34 @@ let phone1 = document.querySelector('#phone');
 let linkedin1 = document.querySelector('#linkedin');
 let github1 = document.querySelector('#github');
 
-
-function sendRequest(){
-  const obj = {
-    palette: 1,
-    name: fullName.value,
-    job: jobTitle.value,
-    phone: phone1.value,
-    email: email1.value,
-    linkedin: linkedin1.value,
-    github: github1.value,
-    photo: 'https://cdn.memegenerator.es/imagenes/memes/full/6/47/6470819.jpg'
-  }
+const obj = {
+  palette: 1,
+  name: fullName.value,
+  job: jobTitle.value,
+  phone: phone1.value,
+  email: email1.value,
+  linkedin: linkedin1.value,
+  github: github1.value,
+  photo: 'https://cdn.memegenerator.es/imagenes/memes/full/6/47/6470819.jpg'
+}
 console.log(obj);
 
- /* console.log('esto nos sale');
+
+function sendRequest(objJson){
   fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
     method: 'POST',
     body: JSON.stringify(objJson),
     headers: {
       'content-type': 'application/json'
-      
-    }
+    },
   })
-  .then(response => response.json())
-  .then(data=>{
-    console.log(data);
-    for (let i=0; i<data.length; i++) {
-      obj[i]=data[i];
-  }}
-  );*/
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.cardURL);
+    });
 }
-submitButton.addEventListener('click', sendRequest);
+submitButton.addEventListener('click', sendRequest(obj));
+
+
+
+
