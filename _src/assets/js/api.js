f1f164296c67be8fa955277e5eaffb346e0896b4
iris paredes <irisparedesjimenez@gmail.com>
2019-04-16 16:38:12 +0200

    add objet whith input

--- _src/assets/js/api.js
+++ _src/assets/js/api.js
@@ -3,24 +3,34 @@
 const submitButton = document.querySelector('.share__button');
 const responseURL = document.querySelector('.response');
 const field = document.querySelector('#field');
-const obj = {
+const fullName = document.querySelector('#fullName');
+const jobTitle = document.querySelector('#jobTitle');
+let email1 = document.querySelector('#email');
+let phone1 = document.querySelector('#phone');
+let linkedin1 = document.querySelector('#linkedin');
+let github1 = document.querySelector('#github');
+
+
+function sendRequest(){
+  const obj = {
     palette: 1,
-    name: 'María García',
-    job: 'Front-end developer',
-    phone: '+34 666666666',
-    email: 'mariagar@example.com',
-    linkedin: 'mariagar',
-    github: 'mariagar',
+    name: fullName.value,
+    job: jobTitle.value,
+    phone: phone1.value,
+    email: email1.value,
+    linkedin: linkedin1.value,
+    github: github1.value,
     photo: 'https://cdn.memegenerator.es/imagenes/memes/full/6/47/6470819.jpg'
   }
+console.log(obj);
 
-function sendRequest(objJson){
-  console.log('esto nos sale');
+ /* console.log('esto nos sale');
   fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
     method: 'POST',
     body: JSON.stringify(objJson),
     headers: {
       'content-type': 'application/json'
+      
     }
   })
   .then(response => response.json())
@@ -29,6 +39,6 @@ function sendRequest(objJson){
     for (let i=0; i<data.length; i++) {
       obj[i]=data[i];
   }}
-  );
+  );*/
 }
-submitButton.addEventListener('click', sendRequest(obj));
+submitButton.addEventListener('click', sendRequest);
