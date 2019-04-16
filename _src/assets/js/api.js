eec2156388231210d793d616493e85a80e8a180c
iris paredes <irisparedesjimenez@gmail.com>
2019-04-16 23:09:32 +0200

    fix congig.JSON and function

---
+++ _src/assets/js/api.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const submitButton = document.querySelector('.share__button');
+const responseURL = document.querySelector('.response');
+const field = document.querySelector('#field');
+const fullName = document.querySelector('#fullName');
+const jobTitle = document.querySelector('#jobTitle');
+let email1 = document.querySelector('#email');
+let phone1 = document.querySelector('#phone');
+let linkedin1 = document.querySelector('#linkedin');
+let github1 = document.querySelector('#github');
+const shareLink = document.querySelector('.share__created--link');
+
+const obj = {
+  palette: 1,
+  name: fullName.value,
+  job: jobTitle.value,
+  phone: phone1.value,
+  email: email1.value,
+  linkedin: linkedin1.value,
+  github: github1.value,
+  photo: 'https://cdn.memegenerator.es/imagenes/memes/full/6/47/6470819.jpg'
+};
+console.log(obj);
+
+
+function sendRequest(obj){
+  fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      shareLink.innerHTML = `<a href="${data.cardURL}">${data.cardURL}</a>`;
+    });
+    
+};
+submitButton.addEventListener('click', sendRequest);
+
+
+
+
