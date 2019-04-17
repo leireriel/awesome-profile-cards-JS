a8f25263565222a30a9a032f7eb182499662d467
iris paredes <irisparedesjimenez@gmail.com>
2019-04-17 11:15:58 +0200

    add field required inm form

--- _src/assets/js/api.js
+++ _src/assets/js/api.js
@@ -11,20 +11,21 @@ let linkedin1 = document.querySelector('#linkedin');
 let github1 = document.querySelector('#github');
 const shareLink = document.querySelector('.share__created--link');
 
-const obj = {
-  palette: 1,
-  name: fullName.value,
-  job: jobTitle.value,
-  phone: phone1.value,
-  email: email1.value,
-  linkedin: linkedin1.value,
-  github: github1.value,
-  photo: 'https://cdn.memegenerator.es/imagenes/memes/full/6/47/6470819.jpg'
-};
-console.log(obj);
 
 
-function sendRequest(obj){
+
+
+function sendRequest(){
+  const obj = {
+    palette: 1,
+    name: fullName.value,
+    job: jobTitle.value,
+    phone: phone1.value,
+    email: email1.value,
+    linkedin: linkedin1.value,
+    github: github1.value,
+    photo: 'https://cdn.memegenerator.es/imagenes/memes/full/6/47/6470819.jpg'
+  };
   fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
     method: 'POST',
     body: JSON.stringify(obj),
@@ -35,10 +36,11 @@ function sendRequest(obj){
     .then(response => response.json())
     .then(data => {
       console.log(data);
+      console.log(data.cardURL);
       shareLink.innerHTML = `<a href="${data.cardURL}">${data.cardURL}</a>`;
     });
     
-};
+}
 submitButton.addEventListener('click', sendRequest);
 
 
