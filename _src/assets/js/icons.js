08d6b20e9adb476ee46b1698ed8866444251a2d4
veroena <veronica.enamorado@gmail.com>
2019-04-25 13:37:27 +0200

    Add last touch fixes

--- _src/assets/js/icons.js
+++ _src/assets/js/icons.js
@@ -4,23 +4,12 @@ let email = document.querySelector('#email');
 let phone = document.querySelector('#phone');
 let linkedin = document.querySelector('#linkedin');
 let github = document.querySelector('#github');
-// //icon
-// let emailIcon = document.querySelector('.email__icon');
-// let phoneIcon = document.querySelector('.phone__icon');
-// let linkedinIcon = document.querySelector('.linkedin__icon');
-// let githubIcon = document.querySelector('.github__icon');
+
 //icon-specific
 const itemEmail = document.querySelector('.list__item--email');
 const itemPhone = document.querySelector('.list__item--phone');
 const itemLinkedin = document.querySelector('.list__item--linkelind');
 const itemGithub = document.querySelector('.list__item--github');
-
-// <!-- <li class="list__item"><a href="" class="card__list--link far fa-envelope email__icon">email</a></li>
-// <li class="list__item"><a href="" class="card__list--link fas fa-mobile-alt phone__icon">mobile</a></li>
-// <li class="list__item"><a href="" class="card__list--link fab fa-linkedin-in linkedin__icon">linkedin</a></li>
-// <li class="list__item"><a href="" class="card__list--link fab fa-github-alt github__icon">github</a></li> -->
-
-
 
 
 //Function to convert input text to icon link
@@ -72,54 +61,3 @@ email.addEventListener('keyup', printEmail);
 phone.addEventListener('keyup', printPhone);
 linkedin.addEventListener('keyup', printLinkedin);
 github.addEventListener('keyup', printGithub);
-
-// //Function to make icons invisible if inputs are empty
-// function invisibleIcon(input, target) {
-//     if (input.value === "") {
-//         target.classList.add('hide__card__list--link');
-//     } else {
-//         target.classList.remove('hide__card__list--link');
-//     }
-// }
-
-
-
-// // Listener for function to convert input text to icon link
-// function printEmail () {
-//     convertToIcon(`mailto:${email.value}`, emailIcon);
-// }
-// function printPhone () {
-//     convertToIcon(`tel:${phone.value}`, phoneIcon);
-// }
-// function printLinkedin () {
-//     convertToIcon(`https://www.linkedin.com/in/${linkedin.value}`, linkedinIcon);
-// }
-// function printGithub () {
-//     convertToIcon(`https://github.com/${github.value}`, githubIcon);
-// }
-
-// email.addEventListener('keyup', printEmail);
-// phone.addEventListener('keyup', printPhone);
-// linkedin.addEventListener('keyup', printLinkedin);
-// github.addEventListener('keyup', printGithub);
-
-
-
-// // Listener for function to make icons invisible if inputs are empty
-// function invisibleEmail () {
-//     invisibleIcon(email, emailIcon);
-// }
-// function invisiblePhone () {
-//     invisibleIcon(phone, phoneIcon);
-// }
-// function invisibleLinkedin () {
-//     invisibleIcon(linkedin, linkedinIcon);
-// }
-// function invisibleGithub () {
-//     invisibleIcon(github, githubIcon);
-// }
-
-// email.addEventListener('keyup', invisibleEmail);
-// phone.addEventListener('keyup', invisiblePhone);
-// linkedin.addEventListener('keyup', invisibleLinkedin);
-// github.addEventListener('keyup', invisibleGithub);
