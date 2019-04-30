c8999a7af8371456532692b477e260aa648e4835
lararb <lararb91@gmail.com>
2019-04-30 12:04:47 +0200

    change icons js

--- _src/assets/js/icons.js
+++ _src/assets/js/icons.js
@@ -11,53 +11,25 @@ const itemPhone = document.querySelector('.list__item--phone');
 const itemLinkedin = document.querySelector('.list__item--linkelind');
 const itemGithub = document.querySelector('.list__item--github');
 
+// const itemLink = document.querySelector('.card__list--link');
 
-//Function to convert input text to icon link
-function printEmail (event) {
+
+
+function print(event, item, text) {
   const gilty = event.currentTarget;
   const value = gilty.value;
 
-  if (value=== '') {
-    itemEmail.innerHTML='';
+  if (value === '') {
+    item.classList.add('hidden');
   } else {
-    itemEmail.innerHTML= `<a href="mailto:${value}" class="card__list--link far fa-envelope email__icon" target="_blank">email</a>`;
+    item.classList.remove('hidden');
+    item.querySelector('.card__list--link').href = `${text}${value}`;
+
   }
 }
 
-function printPhone (event) {
-  const gilty =event.currentTarget;
-  const value = gilty.value;
+email.addEventListener('keyup', (event) => print(event, itemEmail, 'mailto: '));
+phone.addEventListener('keyup', (event) => print(event, itemPhone, 'tel: '));
+linkedin.addEventListener('keyup', (event) => print(event, itemLinkedin, 'https://www.linkedin.com/in/'));
+github.addEventListener('keyup', (event) => print(event, itemGithub, 'https://github.com/'));
 
-  if (value==='') {
-    itemPhone.innerHTML='';
-  } else {
-    itemPhone.innerHTML= `<a href="tel:${value}" class="card__list--link fas fa-mobile-alt phone__icon" target="_blank">mobile</a>`;
-  }
-}
-
-function printLinkedin (event) {
-  const gilty =event.currentTarget;
-  const value = gilty.value;
-
-  if (value==='') {
-    itemLinkedin.innerHTML='';
-  } else {
-    itemLinkedin.innerHTML= `<a href="https://www.linkedin.com/in/${value}" class="card__list--link fab fa-linkedin-in linkedin__icon" target="_blank">linkedin</a>`;
-  }
-}
-
-function printGithub (event) {
-  const gilty =event.currentTarget;
-  const value = gilty.value;
-
-  if (value==='') {
-    itemGithub.innerHTML='';
-  } else {
-    itemGithub.innerHTML= `<a href="https://github.com/${value}" class="card__list--link fab fa-github-alt github__icon" target="_blank">github</a>`;
-  }
-}
-
-email.addEventListener('keyup', printEmail);
-phone.addEventListener('keyup', printPhone);
-linkedin.addEventListener('keyup', printLinkedin);
-github.addEventListener('keyup', printGithub);
